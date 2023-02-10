d217eee175f4337af863acb96bc617c6cb3ef9d5
William Hayes <wilthaye@ut.utm.edu>
2023-02-10 13:08:49 -0600

    Added databases and made them printable

--- DataBase/MainWindow.xaml.cs
+++ DataBase/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.OleDb;
+using System.Windows.Markup;
+using System.Runtime.Remoting.Messaging;
 
 namespace DataBase
 {
@@ -20,9 +23,63 @@ namespace DataBase
     /// </summary>
     public partial class MainWindow : Window
     {
+        OleDbConnection cn;
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            cn = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\Employee.accdb");
+
+            cn.Open();
+        }
+
+        private void See_Assets_Click(object sender, RoutedEventArgs e)
+        {
+            string query = "select * from Assets";
+
+            OleDbCommand cmd = new OleDbCommand(query, cn);
+
+
+            OleDbDataReader reader = cmd.ExecuteReader();
+
+            string data = "";
+
+             while (reader.Read())
+            {
+
+                data += "Item ID: " + reader[0].ToString() + "\n";
+                data += "Name: " + reader[1] + "\n";
+                data += "Employee ID (Owner): " + reader[2].ToString() + "\n";
+                data += "Description: " + reader[3] + "\n\n";
+
+                Textbox1.Text = data;
+             }
+
+
+        }
+
+        private void See_Employee_Click(object sender, RoutedEventArgs e)
+        {
+            int fieldNum = 0;
+
+            string query = "select * from Employees";
+
+            OleDbCommand cmd = new OleDbCommand(query, cn);
+
+            OleDbDataReader reader = cmd.ExecuteReader();
+
+            string data = "";
+
+            while (reader.Read())
+            {
+
+                data += "Employee ID: " + reader[0].ToString() + "\n";
+                data += "Name: " + reader[1] + ", " + reader[2] + "\n\n";
+
+                Textbox2.Text = data;
+            }
         }
     }
 }
